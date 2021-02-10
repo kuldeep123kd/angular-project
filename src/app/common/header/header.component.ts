54f3b56ef26762064c34d5bd0cc07daa5397cf96
Kuldeep Shakya <kuldeep.shakya.kd@gmail.com>
2021-02-11 02:42:53 +0530

    header component implemented and scss styles

---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header-comp',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+}
