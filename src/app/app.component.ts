3d3adb598e25dd77b4d4a949628936586ddcbc49
Kuldeep Shakya <kuldeep.shakya.kd@gmail.com>
2021-02-17 01:21:56 +0530

    angular version updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  someText = 'Hello again';
-
-  clickAngular() {
-    alert("Clicked");
-  }
+  title = 'angular-project1';
 }
