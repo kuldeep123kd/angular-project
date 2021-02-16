a0ac41ddf59d65a3f042aebe41e4af0b1dc8d08b
Kuldeep Shakya <kuldeep.shakya.kd@gmail.com>
2021-02-17 01:55:31 +0530

    scss added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-project1'`, () => {
+  it(`should have as title 'angular-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-project1');
+    expect(app.title).toEqual('angular-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-project1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-project app is running!');
   });
 });
